38558faa1c931a91f65babf7a799afe6dc12602b
Teerasej <teerasej@nextflow.in.th>
2023-12-06 14:30:12 +0700

    ทำ web app กำหนด 2 URL

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 console.log("สวัสดี");
+
+const express = require('express')
+const app = express()
+ 
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/register', function (req, res) {
+  res.send('Register page')
+})
+
+app.listen(3000)
